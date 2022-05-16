fd02f974a8f78ffba6fc827980657b83f32bbf50
askMartyn <martyn@automationlogic.com>
2022-05-16 12:09:41 +0100

    added pihole repo and hostname update

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 LOG_FILE=~/last_run
 WORK_DIR=$(echo "${BASH_SOURCE[0]}" | sed  -e"s/\/run\.sh//")
 cd $WORK_DIR
-unbuffer ansible-playbook default.yml | tee $LOG_FILE
+unbuffer ansible-playbook  default.yml | tee $LOG_FILE
 date>> $LOG_FILE
