c6c98c2c1695753df6cda4016e14d7e832054bc0
askMartyn <martyn@automationlogic.com>
2022-05-16 23:19:58 +0100

    update run.sh with time

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 LOG_FILE=~/last_run
 WORK_DIR=$(echo "${BASH_SOURCE[0]}" | sed  -e"s/\/run\.sh//")
 cd $WORK_DIR
-unbuffer ansible-playbook  default.yml | tee $LOG_FILE
+time unbuffer ansible-playbook  default.yml | tee $LOG_FILE
 date>> $LOG_FILE
