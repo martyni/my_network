c8d1e919f3c73f31f562fc300ba33bfff28908e3
askMartyn <martyn@automationlogic.com>
2022-08-02 15:39:49 +0100

    added forks

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ export TERM=xterm
 LOG_FILE=~/last_run
 WORK_DIR=$(echo "${BASH_SOURCE[0]}" | sed  -e"s/\/run\.sh//")
 cd $WORK_DIR
-time unbuffer ansible-playbook  default.yml | tee $LOG_FILE
+time unbuffer ansible-playbook --forks 4 default.yml | tee $LOG_FILE
 date>> $LOG_FILE
