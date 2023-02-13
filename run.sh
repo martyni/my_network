e075d40f8a3fa4a10fb7d7eb7a7f16752cd503ca
askMartyn <martyn@automationlogic.com>
2023-02-13 16:02:44 +0000

    added proxy role to the run and added vault id

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ export TERM=xterm
 LOG_FILE=~/last_run
 WORK_DIR=$(echo "${BASH_SOURCE[0]}" | sed  -e"s/\/run\.sh//")
 cd $WORK_DIR
-time unbuffer ansible-playbook --forks 8 default.yml | tee $LOG_FILE
+time unbuffer ansible-playbook --vault-id ~/.config/my_ansible/ansible.txt --forks 8 default.yml | tee $LOG_FILE
 date>> $LOG_FILE
